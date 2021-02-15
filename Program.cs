ff9760a2ca5557556feec0b56f5e97a40e437cc8
PeterFerreras <78896409+PeterFerreras@users.noreply.github.com>
2021-02-15 14:00:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+              //Tarea 1
+
+            //Crear un programa que pida números positivos al usuario, y vaya calculando la suma de todos ellos (terminará cuando se teclea un número negativo o cero).
+
+            /*       int q=0;
+              int t;
+              int suma=0;
+
+           do {
+             Console.WriteLine("Coloque un valor:");
+              t= Convert.ToInt32(Console.ReadLine());
+
+              if(t < 0)
+              {
+               Console.WriteLine("La suma de todos los numeros es:"+suma);
+               Environment.Exit(2);
+              }
+              else
+              {
+                suma = suma + t;
+
+
+
+              q++;
+
+
+              }
+            }
+
+            while(t != 0);
+           Console.WriteLine("La suma de los numeros es:"+suma);
+
+            */
+
+
+
+
+
+
+
+            //tarea 2
+
+            //Crea un programa que escriba en pantalla los números del 1 al 10, usando "do..while". 
+
+            /* int q = 1;
+            Console.WriteLine("Se mostraran los numeros de 1 al 10");
+              do {
+
+                   Console.WriteLine(q);
+                   q++;
+
+              }
+
+            while(q<=10);
+            
+              */
+
+
+
+
+
+
+            //tarea 3
+
+            // Crear un programa que muestre los primeros 10 números pares a partir del producto de (10 x 10)    
+
+          /*  int w= 10 * 10;
+            Console.WriteLine("Entre los 10 primeros numeros pares de "+ w +" son :");
+
+
+            for(w = 1; w<=  20; w++)
+            {
+              if (w % 2== 0)
+              {
+                Console.WriteLine(w);
+              }
+            }
+
+            */
+
+
+
+
+
+
+
+             
+             
+             //Tarea 4
+
+            //Crear un programa que muestre las letras de la Z (mayúscula) a la A (mayúscula, descendiendo).
+
+            /* Console.WriteLine("programa que muestre las letras de la Z (mayúscula) a la A (mayúscula, descendiendo)");
+              char w;
+           
+             for(w ='Z'; w >='A'; w--)
+             { 
+             Console.WriteLine(w);
+             }
+             */
+        }
+    }
+
